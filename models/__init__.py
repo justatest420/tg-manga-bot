95b2a1a94fbaf376027069cab50b3f2d1afe55a7
justatest420 <justatestsubject.01@gmail.com>
2025-02-19 15:15:20 +0545

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,13 @@
-from .db import DB, ChapterFile, Subscription, LastChapter, MangaName
+from config import env_vars
+from logger import logger
+
+if env_vars['DB_URL']:
+  from .mdb import *
+  logger.info("Using MongoDB")
+  
+elif env_vars['DATABASE_URL_PRIMARY']:
+  from .db import DB, ChapterFile, Subscription, LastChapter, MangaName
+  logger.info("Using Tembo DB")
+
+else:
+  logger.error("Add Database Link at config.py")
